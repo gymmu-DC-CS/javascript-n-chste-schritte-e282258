c93796ab82307a9661b1cc010ac2d578c5be65a2
e282258 <e282258@sbl.ch>
2023-11-23 12:13:23 +0100

    aufgabe 3 nicht ganz

--- script.js
+++ script.js
@@ -17,4 +17,18 @@ export function aufgabe02(args) {
   const input = args
   const result = input.toUpperCase()
   return result
+  return args.toUpperCase()
+}
+
+export function aufgabe03(args) {
+  const input = args
+  const result = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e" || currentElement === "E") {
+      result++
+    }
+  }
+    return result
 }
