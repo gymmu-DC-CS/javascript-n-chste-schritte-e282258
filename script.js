4ee2df47ad08cfe5e03d5a2089125d1f7dde3252
e282258 <e282258@sbl.ch>
2023-11-28 10:23:09 +0100

    aufgabe 3&4

--- script.js
+++ script.js
@@ -14,21 +14,33 @@ export function aufgabe01(args) {
 }
 
 export function aufgabe02(args) {
-  const input = args
-  const result = input.toUpperCase()
-  return result
-  return args.toUpperCase()
+  const input = args;
+  return input.toUpperCase();
 }
 
-export function aufgabe03(args) {
-  const input = args
-  const result = 0
+export function aufgabe03(text) {
+  let count = 0;
 
-  for (let i = 0; i < input.length; i++) {
-    const currentElement = input[i]
-    if (currentElement === "e" || currentElement === "E") {
-      result++
+  for (let i = 0; i < text.length; i++) {
+    const currentElement = text[i];
+    if (currentElement.toLowerCase() === 'e') {
+      count++;
     }
   }
-    return result
+
+  return count;
+}
+
+export function aufgabe04(args) {
+  if (typeof args !== 'string') {
+      throw new Error('Ungültiger Eingabetyp. Bitte einen Text als Argument übergeben.');
+  }
+
+  const aufgabe04 = args.match(/\S+/g);
+
+  if (!aufgabe04) {
+      return 0;
+  }
+
+  return aufgabe04.length;
 }
